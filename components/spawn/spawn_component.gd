1060dec9509f2ea28cd79aefa9c1ce414b8e6888
Thomas Claessens <thomasclssns01@gmail.com>
2026-07-21 22:17:49 +0200

    chore: move entities to a node2d so y sorting can be enabled

--- components/spawn/spawn_component.gd
+++ components/spawn/spawn_component.gd
@@ -8,16 +8,14 @@ extends Node2D
 
 const GATHERABLE_NODE:= preload('res://scenes/gatherable/Gatherable.tscn')
 
-
 func _ready() -> void:
 	if (gatherable_resources.is_empty()):
 		print('No resources have been configured for this component')
 		return;
 
 	timer.timeout.connect(_on_timer_timeout)
-
 	_check_and_start_timer()
-
+	gatherable_field_component.capacity_freed.connect(_check_and_start_timer)
 	
 func _on_timer_timeout() -> void:
 	if not gatherable_field_component.has_capacity():
@@ -27,9 +25,7 @@ func _on_timer_timeout() -> void:
 	var instance: Gatherable = GATHERABLE_NODE.instantiate()
 	var resource = gatherable_resources.pick_random()
 	instance.gatherable_resource = resource
-
-	instance.tree_exited.connect(_check_and_start_timer)
-
+	print('_on_timer_timeout')
 	gatherable_field_component.spawn_gatherable(instance)
 
 func _check_and_start_timer() -> void:
