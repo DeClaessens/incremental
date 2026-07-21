665fba0571924ea1dad670e2a23199ec8a16e424
Thomas Claessens <thomasclssns01@gmail.com>
2026-07-21 21:17:05 +0200

    chore: v1

--- components/spawn/spawn_component.gd
+++ components/spawn/spawn_component.gd
@@ -10,12 +10,22 @@ const GATHERABLE_NODE:= preload('res://scenes/gatherable/Gatherable.tscn')
 
 
 func _ready() -> void:
-	if (not gatherable_resources.is_empty()):
-		timer.timeout.connect(_on_timer_timeout)
+	if (gatherable_resources.is_empty()):
+		print('No resources have been configured for this component')
+		return;
 
+	gatherable_field_component.capacity_changed.connect(_on_capacity_changed)
+	timer.timeout.connect(_on_timer_timeout)
+
+func _on_capacity_changed(has_capacity: bool) -> void:
+	if has_capacity and timer.is_stopped():
+		return timer.start()
+	
+	if not has_capacity:
+		return timer.stop()
+	
 func _on_timer_timeout() -> void:
-	if (gatherable_field_component.has_capacity()):
-		var instance: Gatherable = GATHERABLE_NODE.instantiate()
-		var resource = gatherable_resources.pick_random()
-		instance.gatherable_resource = resource
-		gatherable_field_component.spawn_gatherable(instance)
+	var instance: Gatherable = GATHERABLE_NODE.instantiate()
+	var resource = gatherable_resources.pick_random()
+	instance.gatherable_resource = resource
+	gatherable_field_component.spawn_gatherable(instance)
